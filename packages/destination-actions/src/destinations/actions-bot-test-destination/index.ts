1489182b6aa19ec70d0a3c6465528b789a821ac9
Varadarajan V <109586712+varadarajan-tw@users.noreply.github.com>
2024-08-30 20:36:20 +0530

    update slug and name

--- packages/destination-actions/src/destinations/actions-bot-test-destination/index.ts
+++ packages/destination-actions/src/destinations/actions-bot-test-destination/index.ts
@@ -4,8 +4,8 @@ import type { Settings } from './generated-types'
 import testAction from './testAction'
 
 const destination: DestinationDefinition<Settings> = {
-  name: 'Actions Bot Test Destination - 1',
-  slug: 'actions-actions-bot-test-destination-1',
+  name: 'Actions Bot Test Destination - 3',
+  slug: 'actions-actions-bot-test-destination-3',
   mode: 'cloud',
   actions: {
     testAction
